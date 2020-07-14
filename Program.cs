6c619b883b627c2dc7a311dd7d5e873873897b76
pedroprestes <pedrosprestes97@gmail.com>
2020-07-14 14:09:31 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso_1_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
